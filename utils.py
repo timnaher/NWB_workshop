e77b108f7117c6e12e2fc1d26ca2d224ae6f129f
timnaher <65108477+timnaher@users.noreply.github.com>
2023-09-07 10:33:54 +0200

    new utils

--- utils.py
+++ utils.py
@@ -58,3 +58,33 @@ def zscore_3d(array_3d):
             zscore_data[i, j] = zscore(array_3d[i, j])
     
     return zscore_data
+
+
+
+
+#___________ plot tools ___________
+
+def plot_movie(data,name):
+    vmin = np.nanmin(data)
+    vmax = np.nanmax(data)
+
+    for i in range(data.shape[2]):  # 100 frames
+        # smooth the current matrix frame
+        frame = data[:,:,i]
+        phase = np.angle(hilbert(frame))
+
+        #frame = ndimage.gaussian_filter(frame, sigma=0.5)
+        im_phase = ax_phase.imshow(phase,vmin=-np.pi, vmax=np.pi,cmap='twilight')
+        ims_phase.append([im_phase])
+
+        im_lfp   = ax_lfp.imshow(frame,vmin=vmin,vmax=vmax,cmap='viridis')
+        ims_lfp.append([im_lfp])
+
+
+    ani_lfp   = animation.ArtistAnimation(fig_lfp, ims_lfp, interval=50, blit=True,repeat_delay=500)
+    ani_phase = animation.ArtistAnimation(fig_phase, ims_phase, interval=50, blit=True,repeat_delay=500)
+
+    writer = PillowWriter(fps=50)
+
+    ani_lfp.save(    f"lfp_{name}.gif", writer=writer)
+    ani_phase.save(f"phase_{name}.gif", writer=writer)
